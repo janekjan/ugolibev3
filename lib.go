5974e459f5eedbce44e25f6be00b94f07ad715ad
janekjan <jas.piskorski@spoko.pl>
2015-03-06 00:07:21 +0100

    Create lib.go
    This is the library.

---
+++ lib.go
@@ -0,0 +1,25 @@
+package tinyev3lib
+
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+//Reads value of n-th plugged sensor(not counting unpluged).
+func ReadSensorValue(n int8) string {
+	sensors := getSensorsList()
+	dat, err := ioutil.ReadFile(sensors[n] + "/value0")
+	errorcheck(err)
+	return string(dat)
+}
+
+func getSensorsList() []string {
+	sensorslist, err := filepath.Glob("/sys/class/lego-sensor/*")
+	errorcheck(err)
+	return sensorslist
+}
+func errorcheck(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
